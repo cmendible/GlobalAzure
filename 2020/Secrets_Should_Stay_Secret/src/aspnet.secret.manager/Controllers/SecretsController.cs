0584e51a1a6e5a974b7c205d227e0ec03332adb9
cmendible <cmendible@gmail.com>
2020-04-17 09:38:38 +0200

    cleanup

---
+++ 2020/Secrets_Should_Stay_Secret/src/aspnet.secret.manager/Controllers/SecretsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace aspnet.secret.manager.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class SecretsController : ControllerBase
+    {
+        private readonly ILogger<SecretsController> _logger;
+
+        private readonly IConfiguration _config;
+
+        public SecretsController(ILogger<SecretsController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpGet("carlos")]
+        public string GetSecret()
+        {
+            return _config["USERSECRET"];
+        }
+    }
+}
